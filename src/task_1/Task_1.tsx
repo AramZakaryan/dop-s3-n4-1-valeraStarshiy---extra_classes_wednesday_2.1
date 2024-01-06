a2220b90e532b358cb821a76c4498eb84cffaf89
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-01-06 11:03:08 +0400

    Result of Exercise (Solved without React.memo or useCallback)

--- src/task_1/Task_1.tsx
+++ src/task_1/Task_1.tsx
@@ -8,36 +8,71 @@ type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) =
 // If change value on input Title component not re-render
 // If change value on input Task_1 component not re-render
 
+// export const Task_1 = () => {
+//
+//     console.log("Task_1 render")
+//
+//     return (
+//         <div style={{...CONTAINER_STYLES} as any}>
+//             <Input/>
+//             <Title title="I am a title"/>
+//         </div>
+//     );
+// };
+//
+// const Input: FC = () => {
+//
+//     console.log("Input render")
+//
+//     const [value, setValue] = useState('');
+//
+//     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//         setValue(e.currentTarget.value);
+//     }
+//
+//     return (
+//         <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
+//     );
+// }
+//
+// const Title = (props: { title: string }) => {
+//
+//     console.log("Title render")
+//
+//     return <h3>{props.title}</h3>
+// }
+
+
 export const Task_1 = () => {
 
     console.log("Task_1 render")
-
-    return (
-        <div style={{...CONTAINER_STYLES} as any}>
-            <Input/>
-            <Title title="I am a title"/>
-        </div>
-    );
-};
-
-const Input: FC = () => {
-
-    console.log("Input render")
 
     const [value, setValue] = useState('');
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.currentTarget.value);
-    }
+    };
 
     return (
-        <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
+        <div style={{...CONTAINER_STYLES} as any}>
+            <Input value={value} onChange={handleChange} />
+            <Title title="I am a title" />
+        </div>
     );
-}
+};
 
 const Title = (props: { title: string }) => {
 
     console.log("Title render")
 
     return <h3>{props.title}</h3>
-}
+};
+
+const Input: FC<PropsType> = ({value, onChange}) => {
+
+    console.log("Input render")
+
+    return (
+        <input type="text" placeholder="Placeholder" value={value} onChange={onChange} />
+    );
+};
