65057cdbc2c39bab966b01fc0ab98840dfa46ce7
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-01-06 09:43:45 +0400

    Result of Exercise (Solved without React.memo or useCallback)

--- src/task_1/Task_1.tsx
+++ src/task_1/Task_1.tsx
@@ -1,35 +1,42 @@
-import {ChangeEvent, FC, useCallback, useState} from 'react';
-import React from "react";
+import React, {ChangeEvent, FC, useCallback, useState} from 'react';
 
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
 
-// type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
+type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
 
 // Task 1
 // If change value on input Title component not re-render
 // If change value on input Task_1 component not re-render
 
 export const Task_1 = () => {
-
+    console.log("Task_1 render")
 
     return (
         <div style={{...CONTAINER_STYLES} as any}>
-            <Input />
+            <Input/>
             <Title title="I am a title"/>
         </div>
     );
 };
 
-const Title = React.memo((props: { title: string }) => <h3>{props.title}</h3>)
+const Input: FC = () => {
 
-const Input = React.memo(() => {
+    console.log("Input render")
+
     const [value, setValue] = useState('');
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.currentTarget.value);
-    };
-        return (
-            <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
-        );
     }
-)
+
+    return (
+        <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
+    );
+}
+
+const Title = (props: { title: string }) => {
+
+    console.log("Title render")
+
+    return <h3>{props.title}</h3>
+}
